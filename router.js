5061bd5803038061a064f0331e5931cc7edf1822
Tripti Mahajan <triptimahajan3@gmail.com>
2023-10-29 14:06:19 +0530

    Initial Commit

---
+++ router.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+const path = require("path");
+
+const  credential = {
+    email : "admin@gmail.com",
+    password : "admin123"
+}
+
+// login user
+router.post('/login', (req, res)=>{
+    if(req.body.email == credential.email && req.body.password == credential.password){
+        req.session.user = req.body.email;
+        res.redirect('/route/dashboard');
+        //res.end("Login Successful...!");
+    }else{
+        res.end("Invalid Username")
+    }
+});
+
+// route for dashboard
+router.get('/dashboard', (req, res) => {
+    if (req.session.user) {
+        const filePath = path.join(__dirname, 'views/dashboard.html');
+        res.sendFile(filePath);
+    } else {
+        res.send("Unauthorize User");
+    }
+});
+// route for logout
+router.get('/logout', (req ,res)=>{
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err);
+            res.send("Error")
+        }else{
+            res.render('base', { title: "Express", logout : "logout Successfully...!"})
+        }
+    })
+})
+
+module.exports = router;
